21c6712dbd98239c530c30a176674aca45620217
Igor Jales <igor.jales@ifood.com.br>
2021-08-25 08:06:12 -0300

    Conexão com API e estilização de Tooltip de erro para inputs inválidos.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
-// import SignIn from './pages/SignIn/index';
-import SignUp from './pages/SignUp/index';
+import SignIn from './pages/SignIn/index';
+// import SignUp from './pages/SignUp/index';
+
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
     <GlobalStyle />
   </>
 );
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -64,7 +64,17 @@ export const Container = styled.div<ContainerProps>`
 export const Error = styled(Tooltip)`
   height: 20px;
   margin-left: 16px;
-  svg: {
+
+  svg {
     margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
